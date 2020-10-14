8afedc886fcb842399cb468050725a1c835702ca
Marlon Lamartine <marlon.lamartine@gmail.com>
2020-10-14 16:48:29 -0300

    Criando evnts e listns para tennt padrao

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use App\Services\TenantService;
+use App\Tenant\Events\TenantCreated;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -75,7 +76,10 @@ class RegisterController extends Controller
         }
         
         $tenantService = app(TenantService::class);
+        $user = $tenantService->make($plan, $data);
 
-        return $tenantService->make($plan, $data);
+        event(new TenantCreated($user));
+
+        return $user;
     }
 }
--- app/Models/Traits/UserACLtrait.php
+++ app/Models/Traits/UserACLtrait.php
@@ -2,12 +2,32 @@
 
 namespace App\Models\Traits;
 
+use App\Models\Tenant;
+
 trait UserACLTrait
 {
     public function permissions()
     {
-        $tenant = $this->tenant;
+        $permissionsPlan = $this->permissionsPlan();
+        $permissionsRole = $this->permissionsRole();
+
+        $permissions = [];
+        foreach($permissionsRole as $permission){
+            if(in_array($permission, $permissionsPlan)){
+                array_push($permissions, $permission);
+            }
+        }
+
+        return $permissions; 
+    }
+
+    public function permissionsPlan() : array
+    {
+        //$tenant = $this->tenant;
+        //$plan = $tenant->plan;
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
         $plan = $tenant->plan;
+
 
         $permissions = [];
 
@@ -19,6 +39,21 @@ trait UserACLTrait
 
         return $permissions;
     }
+
+    public function permissionsRole() : array
+    {
+        $roles = $this->roles()->with('permissions')->get();
+        $permissions = [];
+        
+        foreach($roles as $role){
+            foreach($role->permissions as $permission){
+                array_push($permissions, $permission->name);
+            }
+        }
+
+        return $permissions;
+    }
+
 
     public function hasPermission(string $permissionName) : bool
     {
