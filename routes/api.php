f5efbd23010a9dc18b2ef86ab459e6d4e16fa523
Marlon Lamartine <marlon.lamartine@gmail.com>
2020-10-25 19:49:53 -0300

    Products Api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,21 @@
 <?php
 
-Route::get('/tenants/{uuid}', 'Api\TenantApiController@show');
-Route::get('/tenants', 'Api\TenantApiController@index');
+Route::group([
+    'prefix' => 'v1',
+    'namespace' => 'Api'
+], function () {
+    Route::get('/tenants/{uuid}', 'Api\TenantApiController@show');
+    Route::get('/tenants', 'Api\TenantApiController@index');
 
 
-Route::get('/categories/{url}', 'Api\CategoryApiController@show');
-Route::get('/categories', 'Api\CategoryApiController@categoriesByTenant');
+    Route::get('/categories/{url}', 'Api\CategoryApiController@show');
+    Route::get('/categories', 'Api\CategoryApiController@categoriesByTenant');
 
 
-Route::get('/tables/{identify}', 'Api\TableApiController@show');
-Route::get('/tables', 'Api\TableApiController@tablesByTenant');
+    Route::get('/tables/{identify}', 'Api\TableApiController@show');
+    Route::get('/tables', 'Api\TableApiController@tablesByTenant');
+
+
+    Route::get('/products/{flag}', 'Api\ProductApiController@show');
+    Route::get('/products', 'Api\ProductApiController@productsByTenant');
+});
